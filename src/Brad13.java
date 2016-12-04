1c44e78755b1226c42c47aef65ebf4e7007e70e7
user <user@Z97>
2016-12-04 12:04:21 +0800

    v1

---
+++ src/Brad13.java
@@ -0,0 +1,13 @@
+package tw.brad.bradweb;
+
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Brad13 {
+	public String cal(String x, String y){
+		String result = null;
+		Integer r = Integer.parseInt(x) + Integer.parseInt(y);
+		return r.toString();
+	}
+}
