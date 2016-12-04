7df877a1f63e8bcceb16ab3b8f3049a343e2c163
user <user@Z97>
2016-12-04 13:17:41 +0800

    v5

--- src/tw/brad/bradweb/Brad16.java
+++ src/tw/brad/bradweb/Brad16.java
@@ -2,12 +2,17 @@ package tw.brad.bradweb;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import sun.net.www.content.image.png;
 
 @WebServlet("/Brad16")
 public class Brad16 extends HttpServlet {
@@ -24,6 +29,20 @@ public class Brad16 extends HttpServlet {
 	}
 	
 	private void outHTML(){
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Properties prop = new Properties();
+			prop.setProperty("user", "root");
+			prop.setProperty("password", "root");
+			Connection conn =
+					DriverManager.getConnection(
+							"jdbc:mysql://127.0.0.1/iii", prop);
+			System.out.println("OK");
+		}catch(Exception e){
+			System.out.println(e.toString());
+		}
+
+		
 		out.println("<h1>Brad Big Company</h1><hr>");
 		out.print("<table border='1' width='100%'>");
 		out.print("<tr>");
@@ -31,6 +50,8 @@ public class Brad16 extends HttpServlet {
 		out.print("<th>Account</th>");
 		out.print("<th>Password</th>");
 		out.print("<th>RealName</th>");
+		
+		
 		out.print("</tr>");
 		
 		out.print("</table>");
