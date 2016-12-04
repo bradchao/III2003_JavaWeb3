dd79af88318d72cb3ff9bdd4e1b5882c75f6b6cf
user <user@Z97>
2016-12-04 13:36:53 +0800

    v5

--- src/tw/brad/bradweb/Brad16.java
+++ src/tw/brad/bradweb/Brad16.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Properties;
 
 import javax.servlet.ServletException;
@@ -12,11 +15,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import sun.net.www.content.image.png;
-
 @WebServlet("/Brad16")
 public class Brad16 extends HttpServlet {
 	private PrintWriter out;
+	private Connection conn = null;
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Properties prop = new Properties();
+			prop.setProperty("user", "root");
+			prop.setProperty("password", "root");
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://127.0.0.1/iii", prop);
+		}catch(Exception e){
+		}
+	}
+	
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
 		out = response.getWriter();
@@ -29,19 +47,18 @@ public class Brad16 extends HttpServlet {
 	}
 	
 	private void outHTML(){
-		try{
-			Class.forName("com.mysql.jdbc.Driver");
-			Properties prop = new Properties();
-			prop.setProperty("user", "root");
-			prop.setProperty("password", "root");
-			Connection conn =
-					DriverManager.getConnection(
-							"jdbc:mysql://127.0.0.1/iii", prop);
-			System.out.println("OK");
-		}catch(Exception e){
+		try {
+			PreparedStatement pstmt = 
+					conn.prepareStatement("SELECT * FROM member");
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()){
+				String id = rs.getString(1);
+				System.out.println(id);
+			}
+			
+		} catch (SQLException e) {
 			System.out.println(e.toString());
 		}
-
 		
 		out.println("<h1>Brad Big Company</h1><hr>");
 		out.print("<table border='1' width='100%'>");
