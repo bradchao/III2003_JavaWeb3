643aa34ccc5df9641a69169860aae55ddfa677ea
user <user@Z97>
2016-12-10 11:32:48 +0800

    12101132

---
+++ src/tw/brad/bradweb/Brad20.java
@@ -0,0 +1,75 @@
+package tw.brad.bradweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Brad19")
+public class Brad20 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private PrintWriter out;
+	private Connection conn = null;
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Properties prop = new Properties();
+			prop.setProperty("user", "root");
+			prop.setProperty("password", "root");
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://127.0.0.1/iii", prop);
+			Statement stmt = conn.createStatement();
+			stmt.execute("SET NAME UTF8");
+			
+			
+		}catch(Exception e){
+		}
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		out = response.getWriter();
+		request.setCharacterEncoding("UTF-8");
+		
+		String jsonString = getJSONString(
+				"http://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx");
+
+	}
+	
+	private String getJSONString(String url){
+		String ret = null;
+		try{
+			URL jsonURL = new URL(url);
+			HttpURLConnection conn = 
+					(HttpURLConnection)jsonURL.openConnection();
+			conn.connect();
+			
+			//conn.getInputStream();
+			
+			
+		}catch(Exception ee){}
+		return ret;
+	}
+	
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
