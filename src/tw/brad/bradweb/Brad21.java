1a606010e070adcf92919b696ad7500af0394e67
user <user@Z97>
2016-12-10 13:58:05 +0800

    12101358

--- src/tw/brad/bradweb/Brad21.java
+++ src/tw/brad/bradweb/Brad21.java
@@ -1,6 +1,9 @@
 package tw.brad.bradweb;
 
 import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,5 +15,14 @@ public class Brad21 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("image/jpeg");
+		
+		
+		
+		// Output
+		OutputStream out = response.getOutputStream();
+		ImageIO.write(im, "jpeg", out);
+		out.close();
+	
 	}
 }
