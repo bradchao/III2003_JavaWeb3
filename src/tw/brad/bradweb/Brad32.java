169b83362389a0f413d3e41a5b388ebbd6a9ac52
user <user@Z97>
2016-12-11 11:25:49 +0800

    12111125

--- src/tw/brad/bradweb/Brad32.java
+++ src/tw/brad/bradweb/Brad32.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/Brad32")
 public class Brad32 extends HttpServlet {
@@ -18,7 +19,16 @@ public class Brad32 extends HttpServlet {
 		response.setContentType("text/html;charset=utf-8");
 		out = response.getWriter();
 
-		out.println("Brad32");
+		out.println("Brad32<hr>");
+
+		int Lottery = (int)(Math.random()*49+1);
+		out.print("Lottery:" + Lottery + "<br>");
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("name", "brad");
+		session.setAttribute("lottery", Lottery); // Lottery(int)=>autoboxing=>Integer
+		
+		out.print("<a href='Brad33'>Goto Brad33</a>");
 	
 	}
 
