c935c8b08fe752eff1d029e8da33a4e9d2356e3d
brad <brad@brad.tw>
2017-01-08 11:02:04 +0800

    01081102

--- src/tw/brad/mytags/MyIf.java
+++ src/tw/brad/mytags/MyIf.java
@@ -1,5 +1,22 @@
 package tw.brad.mytags;
 
-public class MyIf {
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class MyIf extends SimpleTagSupport {
+	private boolean test;
+	
+	// Setter method
+	public void setTest(boolean test){
+		this.test = test;
+	}
+	
+	@Override
+	public void doTag() throws JspException, IOException {
+		super.doTag();
+		
+	}
 
 }
