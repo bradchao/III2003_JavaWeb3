0313afac3227753e6c3e98482f674280a5622020
brad <brad@brad.tw>
2017-01-08 12:00:28 +0800

    01081200

--- src/tw/brad/mytags/MyLoop.java
+++ src/tw/brad/mytags/MyLoop.java
@@ -8,8 +8,6 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.JspFragment;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
-import org.apache.jasper.compiler.Node.JspBody;
-
 public class MyLoop extends SimpleTagSupport {
 	private String var;
 	private Collection items;
